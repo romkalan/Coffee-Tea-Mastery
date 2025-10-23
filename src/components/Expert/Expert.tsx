7a2817c7fa62c07600c36f928ca75ca36b118f40
romkalan <romkalan@mail.ru>
2025-10-23 18:24:26 +0300

    add styles for service card and expert card

--- src/components/Expert/Expert.tsx
+++ src/components/Expert/Expert.tsx
@@ -10,11 +10,16 @@ function Expert({expert}: ExpertProps) {
     return (
         <div className={classNames(styles.root)}>
             <p className={classNames(styles.status)}>{expert.status}</p>
-            <div className={classNames(styles.image)}>
-                <img className={classNames(styles.photo)} src={expert.photo} alt={expert.name}/>
+            <div className={classNames(styles.photoContainer)}>
+                <img
+                    className={classNames(styles.photo)}
+                    src={expert.photo}
+                    alt={expert.name}
+                />
             </div>
+            {/*<img className={classNames(styles.photo)} src={expert.photo} alt={expert.name}/>*/}
             <div className={classNames(styles.info)}>
-            <p className={classNames(styles.name)}>{expert.name}</p>
+                <p className={classNames(styles.name)}>{expert.name}</p>
                 <p className={classNames(styles.description)}>{expert.description}</p>
             </div>
         </div>
