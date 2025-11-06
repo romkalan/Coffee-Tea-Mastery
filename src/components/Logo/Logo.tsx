ea80ae2d494e210735d1b1a04e5967b8b0ddd7f7
romkalan <romkalan@mail.ru>
2025-11-06 23:30:23 +0300

    try fix scalling and remove images from assets

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 function Logo() {
     return (
         <Link className={classNames(styles.logo, styles.link)} to={"/"}>
-            <img className={classNames(styles.logoImage)} src={"/logo.png"} alt="Bee Barista Logo"/>
+            <img className={classNames(styles.logoImage)} src={"/images/logo.png"} alt="Bee Barista Logo"/>
             <div className={classNames(styles.logoText)}>
                 <h2 className={classNames(styles.logoTitle)}>Bee Barista</h2>
                 <h3 className={classNames(styles.logoSubTitle)}>Sip Learn Repeat</h3>
