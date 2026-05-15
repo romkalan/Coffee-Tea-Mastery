60247c25fcbcf868edb8eb2188fee8aba3c10840
romkalan <romkalan@mail.ru>
2026-05-15 22:01:06 +0300

    fix: update users mock to match new TUser type

--- src/mocks/users.ts
+++ src/mocks/users.ts
@@ -1,26 +1,28 @@
 import type {TUser} from "../types/user.ts";
 import {generateUUID} from "../utils/utils.ts";
-import {RoleForUser} from "../utils/helpers.ts";
 
-export const courses: TUser[] = [
+export const users: TUser[] = [
     {
         id: generateUUID(),
         name: "Роман",
         email: "romkalan@mail.ru",
-        status: RoleForUser.expert,
+        password: "123",
+        role: "expert",
         photo: "/images/courseImage.png",
     },
     {
         id: generateUUID(),
         name: "Юлия",
         email: "romkalan@mail.ru",
-        status: RoleForUser.student,
+        password: "123",
+        role: "student",
         photo: "/images/courseImage.png",
     },
     {
         id: generateUUID(),
         name: "Денис",
         email: "romkalan@mail.ru",
-        status: RoleForUser.student,
+        password: "123",
+        role: "student",
     },
 ]
