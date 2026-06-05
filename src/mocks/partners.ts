fc79b3472a68ca55785564c72c80dd6a1e57566b
romkalan <romkalan@mail.ru>
2026-06-05 18:24:28 +0300

    fix: update partners - BobCoffer, add URLs

--- src/mocks/partners.ts
+++ src/mocks/partners.ts
@@ -6,25 +6,30 @@ export const partners: TPartner[] = [
         id: generateUUID(),
         name: "Submarine",
         image: "/images/submarineIcon.png",
+        url: "https://sbmrne.ru/",
     },
     {
         id: generateUUID(),
-        name: "Cafe Store",
-        image: "/images/serviceImage.jpg",
+        name: "BobCoffer",
+        image: "/images/bobcofferLogo.svg",
+        url: "https://bobcoffer.com/?ysclid=mq12hdfdwv224081555",
     },
     {
         id: generateUUID(),
         name: "Mahlkoning",
         image: "/images/MahlkoenigLogo.webp",
+        url: "https://www.mahlkoenig.com/",
     },
     {
         id: generateUUID(),
         name: "Hi WATER",
         image: "/images/HiWaterLogo.svg",
+        url: "https://hiwater.ru/",
     },
     {
         id: generateUUID(),
         name: "Tasty Coffee",
         image: "/images/rocketsCoffeeLogo.png",
+        url: "https://shop.tastycoffee.ru/",
     },
 ]
